cc59244a24c6a4ce66f68dc4dd2d4599ade1a08c
Javier Nahuelcheo <sourkuky.gpt@gmail.com>
2024-11-06 06:04:05 -0300

    Parte 2 finalizada

--- laboratorio/admin.py
+++ laboratorio/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Laboratorio, DirectorGeneral, Producto
 
-# Register your models here.
+# Registrar los modelos
+
+
+class LaboratorioAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')  # Columnas a mostrar para Laboratorio
+
+
+class DirectorGeneralAdmin(admin.ModelAdmin):
+    # Columnas a mostrar para DirectorGeneral
+    list_display = ('id', 'nombre', 'laboratorio')
+
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'laboratorio', 'f_fabricacion',
+                    'p_costo', 'p_venta')  # Columnas a mostrar para Producto
+
+
+admin.site.register(Laboratorio, LaboratorioAdmin)
+admin.site.register(DirectorGeneral, DirectorGeneralAdmin)
+admin.site.register(Producto, ProductoAdmin)
