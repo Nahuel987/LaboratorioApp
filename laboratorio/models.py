9cb3442f2fe883197712004b796529754bcc2841
Javier Nahuelcheo <sourkuky.gpt@gmail.com>
2024-11-06 06:11:27 -0300

    Parte 3 finalizada

--- laboratorio/models.py
+++ laboratorio/models.py
@@ -1,27 +1,31 @@
+from django.utils import timezone
 from django.db import models
 
 
 class Laboratorio(models.Model):
     nombre = models.CharField(max_length=255)
+    ciudad = models.CharField(max_length=255)  # Nuevo campo
+    pais = models.CharField(max_length=255)    # Nuevo campo
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}"
 
 
 class DirectorGeneral(models.Model):
     nombre = models.CharField(max_length=255)
     laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
+    especialidad = models.CharField(max_length=255)  # Nuevo campo
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} - {self.laboratorio}"
 
 
 class Producto(models.Model):
     nombre = models.CharField(max_length=255)
     laboratorio = models.ForeignKey(Laboratorio, on_delete=models.CASCADE)
-    f_fabricacion = models.DateField()
+    f_fabricacion = models.DateField(default=timezone.now)
     p_costo = models.DecimalField(max_digits=12, decimal_places=2)
     p_venta = models.DecimalField(max_digits=12, decimal_places=2)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}"
