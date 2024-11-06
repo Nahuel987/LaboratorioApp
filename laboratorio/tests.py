ef879e230cfb0b72368d93620d195382213933f3
Javier Nahuelcheo <sourkuky.gpt@gmail.com>
2024-11-06 06:39:40 -0300

    Parte 5 finalizada

--- laboratorio/tests.py
+++ laboratorio/tests.py
@@ -1,3 +1,53 @@
 from django.test import TestCase
+from .models import Laboratorio
+from django.urls import reverse
 
-# Create your tests here.
+
+class LaboratorioModelTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        # Crea un laboratorio de prueba en la base de datos
+        cls.laboratorio = Laboratorio.objects.create(
+            nombre="Laboratorio de Prueba",
+            ciudad="Ciudad de Prueba",
+            pais="País de Prueba"
+        )
+
+    def test_laboratorio_data_matches(self):
+        # Obtiene el laboratorio desde la base de datos
+        laboratorio = Laboratorio.objects.get(id=self.laboratorio.id)
+
+        # Verifica que los datos coinciden con los creados en setUpTestData
+        self.assertEqual(laboratorio.nombre, "Laboratorio de Prueba")
+        self.assertEqual(laboratorio.ciudad, "Ciudad de Prueba")
+        self.assertEqual(laboratorio.pais, "País de Prueba")
+
+    def test_laboratorio_list_url(self):
+        # Obtiene la URL para la vista de lista de laboratorios
+        url = reverse('laboratorio_list')
+
+        # Realiza una solicitud GET a la URL
+        response = self.client.get(url)
+
+        # Verifica que la respuesta HTTP sea 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+    def test_laboratorio_list_page(self):
+        # Obtiene la URL usando reverse por el nombre de la URL
+        url = reverse('laboratorio_list')
+
+        # Realiza una solicitud GET a la URL
+        response = self.client.get(url)
+
+        # Verifica que la respuesta HTTP sea 200 (OK)
+        self.assertEqual(response.status_code, 200)
+
+        # Verifica que se esté usando la plantilla correcta
+        self.assertTemplateUsed(response, 'laboratorio/laboratorio_list.html')
+
+        # Verifica que el contenido HTML esperado esté en la respuesta
+        # Aquí puedes verificar que el nombre del laboratorio de prueba esté en el HTML
+        self.assertContains(response, "Laboratorio de Prueba")
+        self.assertContains(response, "Ciudad de Prueba")
+        self.assertContains(response, "País de Prueba")
--- practica_final_orm_django/settings.py
+++ practica_final_orm_django/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ry4%4c=-2#6vvmvc^49uhs4qr*-*wg2bwm1(=$22e!xaqd_5ku'
+SECRET_KEY = 'TU CLAVE'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
